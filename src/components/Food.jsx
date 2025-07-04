00242ccc3b7dccc1c261b193e04fc03f936ebeed
Asraf Ali <mdashraf9135@gmail.com>
2025-07-04 15:51:05 +0530

    add new files

---
+++ src/components/Food.jsx
@@ -0,0 +1,34 @@
+import Ngo from '../assets/images/ngo.jpg';
+import Donate from '../assets/images/donate.jpg';
+const Food = [
+    {
+        id:1,
+        img:Ngo,
+        name:"Public Help",
+        desc:"Help 24 / 7",
+        btn:"Donate Now"
+    },
+    {
+        id:2,
+        img:Donate,
+        name:"Public Help",
+        desc:"Help 24 / 7",
+        btn:"Donate Now"
+    },
+    {
+        id:3,
+        img:Ngo,
+        name:"Public Help",
+        desc:"Help 24 / 7",
+        btn:"Donate Now"
+    },
+    {
+        id:4,
+        img:Ngo,
+        name:"Public Help",
+        desc:"Help 24 / 7",
+        btn:"Donate Now"
+    }
+];
+
+export default Food
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Donate from '../assets/images/donate.jpg';
+import { NavLink } from 'react-router-dom';
+import Food from './Food';
 
 const Main = () => {
+
+    const [food] = useState(Food);
+
   return (
     <>
         <section>
@@ -21,6 +26,59 @@ const Main = () => {
                     <div className='col-lg-4 col-md-4 col-sm-12'>
                         <img src={Donate} alt={Donate} height={'300px'} width={'100%'}></img>
                     </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className='container mt-4 mb-5'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='ngo'>
+                            <div className='row'>
+                                <div className='col-lg-4 col-md-4 col-sm-12'>
+                                    <div className='first-icons'>
+                                        <i class="fas fa-users"></i> <span>800,000+ people fed</span>
+                                    </div>
+                                </div>
+                                <div className='col-lg-4 col-md-4 col-sm-12'>
+                                    <div className='first-icons'>
+                                        <i class="fas fa-headphones-alt"></i> <span>24 X 7 Customer support</span>
+                                    </div>
+                                </div>
+                                <div className='col-lg-4 col-md-4 col-sm-12'>
+                                    <div className='first-icons'>
+                                        <i class="fas fa-medal"></i> <span>ISO Certified</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section>
+            <div className='container mt-5 mb-5'>
+                <h4 className='text-white text-center'>Our Services</h4>
+                <div className='row'>
+                    {
+                        food.map((item) =>{
+                            return(
+                                <div className='col-lg-3 col-md-3 col-sm-12 mt-3'>
+                                    <div className='testemonials-card'>
+                                        <div className='img'>
+                                            <img src={item.img} alt={item.img} height={'200px'} width={'100%'}></img>
+                                        </div>
+                                        <h6 className='text-center pt-3'>{item.name}</h6>
+                                        <p className='text-center'>{item.desc}</p>
+                                        <div style={{display:'block', textAlign:'center'}}>
+                                            <NavLink to={''}><button className='btn btn-warning'>{item.btn}</button></NavLink>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </section>
